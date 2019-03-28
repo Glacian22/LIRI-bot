2ed47eac5a7c14a846141a162dd0b947a7e64106
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-03-27 20:32:35 -0700

    added dowhatitsays, all done

--- liri.js
+++ liri.js
@@ -3,21 +3,25 @@ var keys = require("./keys.js");
 var bands = require("./bands")
 var spotify = require("./spotify")
 var movie = require("./movie")
+var fs = require('fs');
 
 var command = process.argv[2]
+var input = getInput();
 
-if (command === "concert-this") {
-    var artist = getArg();
-    bands(artist);
-} else if (command === "spotify-this-song") {
-    var song = getArg();
-    spotify(song);    
-}else if (command === "movie-this"){
-    var movieTitle = getArg();
-    movie(movieTitle)
+runLiri(command, input)
+
+function runLiri(command, input) {
+    if (command === "concert-this") {
+        bands(input);
+    } else if (command === "spotify-this-song") {
+        spotify(input);
+    } else if (command === "movie-this") {
+        movie(input);
+    } else if (command === "do-what-it-says") {
+        doWhatItSays();
+    }
 }
-
-function getArg(){
+function getInput() {
     var arg = "";
     //loop over process.argv for index 3+ to get the full argument
     for (var i = 3; i < process.argv.length; i++) {
@@ -28,4 +32,18 @@ function getArg(){
         }
     }
     return arg;
+}
+
+function doWhatItSays() {
+    fs.readFile('random.txt', "utf8", function (err, data) {
+        if (err) {
+            return console.log("Error:", err)
+        }
+        //turn the file string into variables to send into runLiri()
+        var split = data.split(",");
+        var command = split[0];
+        var input = split[1].slice(1, split[1].length - 1)
+
+        runLiri(command, input)
+    })
 }
